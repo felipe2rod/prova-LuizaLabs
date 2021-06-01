3614d81ab43e4fa6860b0354b48067178522084b
Felipe Rodrigues <felipe2rod@gmail.com>
2021-06-01 13:47:00 -0300

    fix: add fillable to Clients Model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -10,4 +10,11 @@ class Client extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+     protected $fillable = [
+        'name',
+        'cpf',
+        'email',
+        'sex'
+    ];
 }
