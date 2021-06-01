7c12b360e645cbc6af42f03322e734eac0ff0fc1
Felipe Rodrigues <felipe2rod@gmail.com>
2021-06-01 20:28:16 -0300

    feat: add migration

---
+++ database/seeders/ClientsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Client;
+
+
+class ClientsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Client::factory()
+            ->count(50)
+            ->create();
+    }
+}
