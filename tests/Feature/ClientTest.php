4e44e00aab34fce2855363b805ce1db96ac97f56
Felipe Rodrigues <felipe2rod@gmail.com>
2021-06-01 19:05:46 -0300

    test: user update tests

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -35,7 +35,7 @@ class ClientTest extends TestCase
 
     }
 
-    public function test_post_success()
+    public function test_post__success()
     {
         $clientData = [
             'name' => 'Felipe Rodrigues Fernandes',
@@ -53,7 +53,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_required_name()
+    public function test_post_invalid_required_name()
     {
         $clientData = [
             'name' => null,
@@ -74,7 +74,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_type_name()
+    public function test_post_invalid_type_name()
     {
         $clientData = [
             'name' => 1,
@@ -95,7 +95,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_max_size_name()
+    public function test_post_invalid_max_size_name()
     {
         $clientData = [
             'name' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
@@ -116,7 +116,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_required_cpf()
+    public function test_post_invalid_required_cpf()
      {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -137,7 +137,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_type_cpf()
+    public function test_post_invalid_type_cpf()
     {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -158,7 +158,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_max_cpf()
+    public function test_post_invalid_max_cpf()
      {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -179,7 +179,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_min_cpf()
+    public function test_post_invalid_min_cpf()
     {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -201,7 +201,7 @@ class ClientTest extends TestCase
     }
 
 
-    public function test_invalid_unique_cpf()
+    public function test_post_invalid_unique_cpf()
     {
         $Client = Client::factory()->create();
 
@@ -224,7 +224,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_required_email()
+    public function test_post_invalid_required_email()
      {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -245,7 +245,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_type_email()
+    public function test_post_invalid_type_email()
     {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -266,7 +266,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_max_email()
+    public function test_post_invalid_max_email()
      {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -289,7 +289,7 @@ class ClientTest extends TestCase
 
 
 
-    public function test_invalid_unique_email()
+    public function test_post_invalid_unique_email()
     {
         $Client = Client::factory()->create();
 
@@ -312,7 +312,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_required_sex()
+    public function test_post_invalid_required_sex()
     {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -333,7 +333,7 @@ class ClientTest extends TestCase
         ]);
     }
 
-    public function test_invalid_type_sex()
+    public function test_post_invalid_type_sex()
     {
         $clientData = [
             'name' => 'Felipe Rodrigues',
@@ -386,6 +386,242 @@ class ClientTest extends TestCase
                     'meta' => 'No query results for model [App\\Models\\Client] 000'
                 ]
             ]);
+    }
+
+
+    public function test_put_success()
+    {
+        $Client = Client::factory()->create();
+
+        $clientData = [
+            'name' => "Novo nome",
+            'email' => 'novoemail@gmail.com',
+            'cpf' => '111.311.111-13',
+            'sex' => 'female'
+        ];
+
+        $response = $this->put("/api/clients/{$Client->id}", $clientData);
+        $response
+            ->assertStatus(JsonResponse::HTTP_CREATED)
+            ->assertJson([
+                'success' => true,
+                'data' => 'Successfully updated'
+        ]);
+
+        $clientData['sex'] = $clientData['sex'] ==  'male' ? 'Masculino': 'Feminino';
+        $response = $this->get("/api/clients/{$Client->id}");
+        $response
+            ->assertStatus(JsonResponse::HTTP_CREATED)
+            ->assertJson([
+                'success' => true,
+                'data' => array_merge(['id' => $Client->id],$clientData)
+        ]);
+    }
+
+
+    public function test_put_invalid_type_name()
+    {
+        $Client = Client::factory()->create();
+        $clientData = [
+            'name' => null,
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'name' => ['O campo nome deve ser uma string']
+                ]
+        ]);
+    }
+
+    public function test_put_invalid_max_size_name()
+    {
+        $Client = Client::factory()->create();
+        $clientData = [
+            'name' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'name' => ['O campo nome deve ter no maximo 45 caracteres']
+                ]
+        ]);
+    }
+
+    public function test_put_invalid_type_cpf()
+    {
+        $Client = Client::factory()->create();
+
+        $clientData = [
+            'cpf' => 11111111111111,
+        ];
+
+        $response = $this->put('/api/clients/{$Client->id}', $clientData);
+        $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'cpf' => ['O campo cpf deve ser uma string']
+                ]
+        ]);
+    }
+
+    public function test_put_invalid_max_cpf()
+    {
+        $Client = Client::factory()->create();
+
+        $clientData = [
+            'cpf' => '111111111111111',
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'cpf' => ['O campo cpf deve ter 14 caracteres']
+                ]
+        ]);
+    }
+
+    public function test_put_invalid_min_cpf()
+    {
+        $Client = Client::factory()->create();
+
+        $clientData = [
+            'cpf' => '1',
+        ];
+
+        $response = $this->put('/api/clients/{$Client->id}', $clientData);
+        $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'cpf' => ['O campo cpf deve ter 14 caracteres']
+                ]
+        ]);
+    }
+
+
+    public function test_put_invalid_unique_cpf()
+    {
+        $Client = Client::factory()->create();
+        $Client2 = Client::factory()->create();
+
+        $clientData = [
+            'name' => 'Felipe Rodrigues',
+            'email' => 'felipe1rod@gmail.com',
+            'cpf' => $Client2->cpf,
+            'sex' => 'male'
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'cpf' => ['O cpf informado ja foi cadastrado']
+                ]
+        ]);
+    }
+
+    public function test_put_invalid_type_email()
+    {
+        $Client = Client::factory()->create();
+
+        $clientData = [
+            'email' => 1111,
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'email' => ['O campo email deve ser uma string']
+                ]
+        ]);
+    }
+
+    public function test_put_invalid_max_email()
+     {
+        $Client = Client::factory()->create();
+
+        $clientData = [
+            'email' => 'felipe1roaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad@gmail.com',
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'email' => ['O campo email deve ter no maximo 45 caracteres']
+                ]
+        ]);
+    }
+
+
+
+    public function test_put_invalid_unique_email()
+    {
+        $Client = Client::factory()->create();
+        $Client2 = Client::factory()->create();
+
+        $clientData = [
+            'email' => $Client2->email,
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'email' => ['O email informado ja foi cadastrado']
+                ]
+        ]);
+    }
+
+    public function test_put_invalid_type_sex()
+    {
+        $Client = Client::factory()->create();
+        $clientData = [
+            'sex' => 'invalid'
+        ];
+
+         $response = $this->put('/api/clients/{$Client->id}', $clientData);
+         $response
+            ->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Validation failed',
+                'data' => [
+                    'sex' => ['O sexo informado é invalido']
+                ]
+        ]);
     }
 
 }
